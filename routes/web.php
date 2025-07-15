90ccebaf8f4645190307e6c8b7f49f0f3ced6e03
Ryotaro Yamamoto <yamatako64@gmail.com>
2025-07-15 15:03:45 +0800

    name

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 Auth::routes();
 
 Route::group(['middleware' => 'auth'], function () {
-    Route::get('/', [HomeController::class, 'index'])->name('index');
+    Route::get('/', [HomeController::class, 'index'])->name('index');//Ryotaro
     Route::get('/people', [HomeController::class, 'search'])->name('search'); //Riko
 
     //Admin
