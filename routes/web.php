e84fb932002118ec52004d81f201520b2f8fd038
Arei-Nakata <nakata1588@gmail.com>
2025-07-15 15:11:48 +0800

    test

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 
 Auth::routes();
 
-Route::group(['middleware' => 'auth'], function () {
+Route::group(['middleware' => 'auth'], function () { //arei
     Route::get('/', [HomeController::class, 'index'])->name('index');//Ryotaro
     Route::get('/people', [HomeController::class, 'search'])->name('search'); //Riko
 
