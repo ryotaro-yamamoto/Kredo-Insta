6533059b659750e7d3a049d2e758814cbcbba755
Ryotaro Yamamoto <yamatako64@gmail.com>
2025-07-15 15:42:25 +0800

    arei

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 
 Auth::routes();
 
-Route::group(['middleware' => 'auth'], function () { //arei
+Route::group(['middleware' => 'auth'], function () { //AREI
     Route::get('/', [HomeController::class, 'index'])->name('index');//Ryotaro
     Route::get('/people', [HomeController::class, 'search'])->name('search'); //Riko Riko2 Riko3
 
