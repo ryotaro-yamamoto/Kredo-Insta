c4adda3b0f4e9b30bbb8878cb1b22815a2fe5cea
RIKO20000111 <ric01112000@gmail.com>
2025-07-16 15:41:35 +0800

    comment

--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,10 @@ use Illuminate\Support\Facades\Route;
 
 Auth::routes();
 
-Route::group(['middleware' => 'auth'], function () { //arei
-    Route::get('/', [HomeController::class, 'index'])->name('index');//Ryotaro
-    Route::get('/suggestions', [HomeController::class, 'suggestions'])->name('suggestions.index');
-    Route::get('/people', [HomeController::class, 'search'])->name('search'); //Riko
+Route::group(['middleware' => 'auth'], function () { 
+    Route::get('/', [HomeController::class, 'index'])->name('index');
+    Route::get('/suggestions', [HomeController::class, 'suggestions'])->name('suggestions.index');//NEW(RIKO)
+    Route::get('/people', [HomeController::class, 'search'])->name('search');
 
     //Admin
     Route::group(['prefix' => 'admin', 'as' => 'admin.', 'middleware' => 'admin'], function () {
