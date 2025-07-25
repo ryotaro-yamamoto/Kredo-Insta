f929181a48bb64a3dd6508ef2bb68a1d53f1b3c4
Ryotaro Yamamoto <yamatako64@gmail.com>
2025-07-25 15:13:43 +0800

    a

--- routes/web.php
+++ routes/web.php
@@ -150,10 +150,13 @@ Route::group(['middleware' => 'auth'], function () {
     Route::delete('/follow/{user_id}/destroy', [FollowController::class, 'destroy'])->name('follow.destroy');
 
     Route::get('/secret-delete-story', function(){
-        $story = Story::truncate();
-
-        return $story;
-
+        $story = Story::find(1);
+    
+        if($story){
+            $story->delete();
+            return "Deleted Successfully";
+        }
+        return "Story not found";
     });
 
     Route::get('/secret-show-story', function(){
